4b539d4b9507e0f97a565cdd334b4855c8d6d109
Alik Kumar Ghosh <33929979+Alik-Kumar-Ghosh@users.noreply.github.com>
2023-09-17 16:21:01 +0530

    changes in alert

--- extension/notification/notification.js
+++ extension/notification/notification.js
@@ -1,8 +1,10 @@
 document.getElementById('openButton').addEventListener('click', function() {
-    // Handle "Open" button click action here
+    window.close() ;
+    return 0;
   });
   
   document.getElementById('dismissButton').addEventListener('click', function() {
-    // Handle "Dismiss" button click action here
+    window.close() ;
+    return 1;
   });
   
